1e4f17a8600cb95053c516ebc0dad408e6c5f260
Joseph Hocking <jhocking@newarteest.com>
2019-10-11 22:42:14 -0500

    adjust for blender 2.8 and change to add-on

--- dropper4blend.py
+++ dropper4blend.py
@@ -1,10 +1,16 @@
-'''
-Dropper
-by Joseph Hocking www.newarteest.com
-GPL license https://www.blender.org/support/faq/
+bl_info = {
+	"name": "Dropper",
+	"description": "Saves info about objects in the scene.",
+	"author": "Joseph Hocking www.newarteest.com",
+	"blender": (2, 80, 0),
+	"location": "File > Export > Dropper",
+	"category": "Import-Export",
+}
 
-written in Blender 2.77
-open in Text Editor view, then Run Script
+'''
+install in Edit > Preferences > Add-ons
+once installed: File > Export > Dropper
+GPL license https://www.blender.org/support/faq/
 
 Saves info about objects in the scene:
 name, position, rotation, scale, custom properties
@@ -27,8 +33,8 @@ def write_data(context, filepath, format):
     print("writing scene data...")
     
     data = {
-        'OPT_JSON':build_json(),
-        'OPT_XML':build_xml(),
+        'OPT_JSON': build_json(),
+        'OPT_XML': build_xml(),
     }[format]
     
     f = open(filepath, 'w', encoding='utf-8')
@@ -118,20 +124,20 @@ class Dropper(Operator, ExportHelper):
     # ExportHelper mixin class uses this
     filename_ext = ".txt"
 
-    filter_glob = StringProperty(
-            default="*.txt",
-            options={'HIDDEN'},
-            maxlen=255,  # Max internal buffer length, longer would be clamped.
-            )
+    filter_glob: StringProperty(
+        default="*.txt",
+        options={'HIDDEN'},
+        maxlen=255,  # Max internal buffer length, longer would be clamped.
+    )
 
     # options menu next to the file selector
-    data_format = EnumProperty(
-            name="Data Format",
-            description="Choose the data format",
-            items=(('OPT_JSON', "JSON", "JavaScript Object Notation"),
-                   ('OPT_XML', "XML", "eXtensible Markup Language")),
-            default='OPT_JSON',
-            )
+    data_format: EnumProperty(
+        name="Data Format",
+        description="Choose the data format",
+        items=(('OPT_JSON', "JSON", "JavaScript Object Notation"),
+               ('OPT_XML', "XML", "eXtensible Markup Language")),
+        default='OPT_JSON',
+    )
 
     def execute(self, context):
         return write_data(context, self.filepath, self.data_format)
@@ -143,16 +149,16 @@ def menu_func_export(self, context):
 
 def register():
     bpy.utils.register_class(Dropper)
-    bpy.types.INFO_MT_file_export.append(menu_func_export)
+    bpy.types.TOPBAR_MT_file_export.append(menu_func_export)
 
 
 def unregister():
     bpy.utils.unregister_class(Dropper)
-    bpy.types.INFO_MT_file_export.remove(menu_func_export)
+    bpy.types.TOPBAR_MT_file_export.remove(menu_func_export)
 
 
 if __name__ == "__main__":
     register()
 
     # test call
-    bpy.ops.dropper.scene_text('INVOKE_DEFAULT')
+    #bpy.ops.dropper.scene_text('INVOKE_DEFAULT')
